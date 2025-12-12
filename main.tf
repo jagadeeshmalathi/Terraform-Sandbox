5a789d3c0c225ff2560a3b9247a70e3358c93441
pradipta <pradiptasundarnayak5@gmail.com>
2025-12-12 19:39:16 +0530

    added resource and output

--- main.tf
+++ main.tf
@@ -1,43 +1,36 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "my_vpc"
-  }
-}
-
-# Create an Internet Gateway
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "my-igw"
-  }
-}
-
-# Create a Public Subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = var.public_subnet_cidr
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (for us-east-1)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
-  key_name      = "Ec2_App_Key" # Replace with your key pair name
+ami = "ami-068c0051b15cdb816"
+instance_type = "t2.micro"
+key_pair = "Jenkins-Slave"
+vpc_security_group_ids = [aws_security_group.my_sg.id]
+}
+
+
+resource "aws_security_group" "my_sg" {
+  name        = "my-terraform-sg"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      =  aws_vpc.my-vpc.id
+
+
+  # Inbound rules
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rules
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "my-ec2-instance"
+    Name = "terraform-sg"
   }
 }
-resource "aws_ec2_instance_state" "my_ec2" {
-  instance_id = aws_instance.my_ec2.id
-  state       = "stopped"
-}
-

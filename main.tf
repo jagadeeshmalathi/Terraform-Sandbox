568a278439944147defa420a4b085208866f61c3
aws_pritam <88572358+pritam992@users.noreply.github.com>
2025-12-15 18:58:28 +0530

    Add parameters to EC2 module in main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 module "my_ec2" {
-  source = "./modules/ec2"
+  source      = "./modules/ec2"
+
+count         = 2
+ami           = "ami-0cca134ec43cf708f"
+instance_type = "t3.small"
+sg_name       = "my-tf-sg"
 }

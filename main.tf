c2fec0c863f53467d85ae92363077deb9a4c19bd
Jagadeesh Malathi <jack@MacBookPro.cable.rcn.com>
2025-12-15 09:18:36 -0500

    Updated the hardcoded values

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "my_ec2" {
   source      = "./modules/ec2"
 
-count-num         = 2
-ami           = "ami-0cca134ec43cf708f"
-instance_type = "t3.small"
-sg_name       = "my-tf-sg"
+count-num     = var.count-num
+ami           = var.ami
+instance_type = var.instance_type
+sg_name       = var.sg_name
 }
---
+++ value.tfvars
@@ -0,0 +1,4 @@
+count-num     = 2
+ami           = "ami-0cca134ec43cf708f"
+instance_type = "t3.micro"
+sg_name       = "pradipta-sg"

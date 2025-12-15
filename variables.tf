dd4c43b9c49c5897875812453773f56a71e462f9
aws_pritam <88572358+pritam992@users.noreply.github.com>
2025-12-15 18:59:32 +0530

    Add variables for EC2 configuration

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+ description = "ami id"
+ type = string
+}
+
+variable "instance_type" {
+ description = "instance_type of ec2"
+ type    = string
+}
+
+variable "count" {
+ description = "creates number of ec2"
+ type = number
+}
+
+variable "sg_name" {
+ description = "Security group for my ec2"
+ type        = string
+}
